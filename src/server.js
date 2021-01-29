3473021efadca48250e15a88d3dd1e964c081974
MahdiMaarouf2016 <medmahdi.maarouf@gmail.com>
2021-01-29 04:11:52 +0100

    server init

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const app = express();
 const port = 8000;
+
 app.use('/dashboard', express.static(path.join(__dirname, '../client/')))
 app.get('/', (req, res) => { res.redirect("/dashboard") });
 

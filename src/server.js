64adbdbecff397118f311e8a7dcf20a8f67875e7
MahdiMaarouf2016 <medmahdi.maarouf@gmail.com>
2021-01-29 04:04:55 +0100

    fix-c

--- src/server.js
+++ src/server.js
@@ -3,9 +3,7 @@ const path = require('path')
 const app = express();
 const port = 8000;
 app.use('/dashboard', express.static(path.join(__dirname, '../client/')))
-app.get('/', (req, res) => {
-    res.redirect("/dashboard")
-});
+app.get('/', (req, res) => { res.redirect("/dashboard") });
 
 app.listen(port, () => {
     console.log(`themify listening on port ${port}!`)

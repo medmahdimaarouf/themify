bd14df46ebf12cbd7ce93e23cc7ac2fef9eefa68
MahdiMaarouf2016 <medmahdi.maarouf@gmail.com>
2021-01-29 04:12:58 +0100

    discare-file

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const app = express();
 const port = 8000;
 
-app.use('/dashboard', express.static(path.join(__dirname, '../client/')))
+app.use('/dashboard', express.static(path.join(__dirname, '../client/')));
 app.get('/', (req, res) => { res.redirect("/dashboard") });
 
 app.listen(port, () => {
